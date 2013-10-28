a45a56e87fc9e2c707db3e790aabffbf2ce19597
Rupa Shankar <rupsshankar@gmail.com>
2013-10-28 18:38:13 -0400

    Splits based on whitespace

--- src/interpreter/core.clj
+++ src/interpreter/core.clj
@@ -1,7 +1,13 @@
 (ns interpreter.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn tokenize
+  [string]
+  (let [spaced-out (str/replace string #"[()]" #(str " " % " "))]
+    (filter (fn[x] (not (empty? x))) (str/split spaced-out #"\\s"))))
