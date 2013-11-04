26a36e57469f4129d02064bfefc975c8bfa25228
Rupa Shankar <rupsshankar@gmail.com>
2013-11-04 15:04:44 -0500

    Working vectorify function but without paren error checking

--- src/interpreter/core.clj
+++ src/interpreter/core.clj
@@ -2,12 +2,80 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+;; basically implements the built-in "read-string" function
+;; need to figure out error checking with parentheses
+(defn vectorify
+  [tokens]
+  (loop [i 0
+         stack []]
+   (if (and (>= i (count tokens))
+    (first stack)
+    (do (let [t (nth tokens i)]
+        (cond 
+          (= t "(") (recur (inc i) (into [] (cons [] stack)))
+            (= t ")")
+            (do (let [top-vec (first stack)
+                      stack (rest stack)]
+                  (if (empty? stack)
+                    top-vec
+                    (do (let [next-vec (first stack)
+                              stack (rest stack)]
+                          (recur (inc i) (into [] (cons (conj next-vec top-vec) stack))))))))
+            :else 
+            (do (let [curr-vec (first stack)]
+                (recur (inc i) (assoc stack 0 (into [] (conj curr-vec t))))))))))))
+
+; (defn vectorify
+;   [tokens]
+;   (loop [i 0
+;    stack []]
+;    (println "BEGINNING")
+
+;    (println "STACK")
+;    (println stack)
+;    (println "I = " i)
+
+;    (if (and (>= i (count tokens))
+;     (first stack)
+;     (do
+;       (let [t (nth tokens i)]
+;         (println t)
+;         (cond 
+;           (= t "(") (recur (inc i) (into [] (cons [] stack)))
+;             (= t ")")
+;             (do 
+;               (let [top-vec (first stack)]
+;                 (println "TOP-VEC " top-vec)
+;                 (let [stack (rest stack)]
+;                   (println "stack = " stack)
+;                   (if (empty? stack)
+;                     top-vec
+;                     (do
+;                       (let [next-vec (first stack)]
+;                         (println "stack before = " stack)
+;                         (println "NEXT_VEC " next-vec)
+;                         (let [stack (rest stack)]
+;                           (println "stack after  = " stack)
+;                           (recur (inc i) (into [] (cons (conj next-vec top-vec) stack))))))))))
+;             :else 
+;             (do 
+;               (let [curr-vec (first stack)]
+;                 (recur (inc i) (assoc stack 0 (into [] (conj curr-vec t))))))))))))
+
+  
+
+(defn remove-blanks
+  [string]
+    (filter (fn[x] (not (empty? x))) (str/split string #"\\s")))
 
 (defn tokenize
   [string]
-  (let [spaced-out (str/replace string #"[()]" #(str " " % " "))]
-    (filter (fn[x] (not (empty? x))) (str/split spaced-out #"\\s"))))
+  (let [spaced-out (remove-blanks (str/replace string #"[()]" #(str " " % " ")))]
+    spaced-out))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!")
+  (println (tokenize "(+ 1 2)")))
+
