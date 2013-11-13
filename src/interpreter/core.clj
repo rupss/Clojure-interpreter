5cb4edbdf699ac4831a74a6468530f131d8ec877
Rupa Shankar <rupsshankar@gmail.com>
2013-11-13 16:58:21 -0500

    Began to add support for a few builtins (map, filter, conj, cons, first, last, not, rest) and split insert-into-top into a multimethod

--- src/interpreter/core.clj
+++ src/interpreter/core.clj
@@ -4,6 +4,19 @@
 
 (def operator-map {"+" + "-" - "*" * "/" /})
 (def bool-map {"true" true "false" false})
+(def builtin-map {
+  "map" map
+  "filter" filter
+  "conj" conj
+  "cons" cons
+  "first" first
+  "last" last
+  "not" not
+  "rest" rest})
+
+(defn is-vector?
+  [item]
+  (= clojure.lang.PersistentVector (type item)))
 
 ; (defn vectorify
 ;   [tokens]
@@ -64,15 +77,50 @@
   (let [operator (operator-map token)
         str-literal (str-literal token)
         num-literal (num-literal token)
-        bool (bool-map token)]
+        bool (bool-map token)
+        builtin (builtin-map token)]
   (cond
     (not (nil? operator)) {:type :operator :value operator}
     (not (nil? str-literal)) {:type :literal :value str-literal}
     (not (nil? num-literal)) {:type :literal :value num-literal}
     (not (nil? bool)) {:type :bool :value bool}
+    (not (nil? builtin)) {:type :builtin :value builtin}
     :else {:type :identifier :value token})))
 
 ;; TOKENIZING
+
+(defn is-list-item?
+  [item]
+  ; (println "#### IS-LIST-ITEM? #####")
+  ; (println item)
+  ; (println (type item))
+  (and (= clojure.lang.PersistentArrayMap (type item)) (= (item :type) :list)))
+
+(defn get-struct-type
+  [curr-struct tagged-item]
+  (cond
+   (is-vector? curr-struct) :vector
+   (is-list-item? curr-struct) :list))
+
+(defmulti insert-into-top get-struct-type)
+
+(defmethod insert-into-top :vector
+  [curr-struct tagged-item]
+  (into [] (conj curr-struct tagged-item)))
+
+(defmethod insert-into-top :list
+  [curr-struct tagged-item]
+  (assoc curr-struct :value (conj (curr-struct :value) tagged-item)))
+
+(defmulti my-conj get-struct-type)
+
+(defmethod my-conj :vector
+  [curr-struct to-add]
+  (conj curr-struct to-add))
+
+(defmethod my-conj :vector
+  [curr-struct to-add]
+  )
 
 ;; basically implements the built-in "read-string" function
 ;; need to figure out error checking with parentheses
@@ -89,35 +137,45 @@
           (println "ERROR")
           (System/exit -1)))))
     (do (let [t (nth tokens i)]
+         (println "token")
+         (println t)
         (cond 
           (= t "(") (recur (inc i) (into [] (cons [] stack)))
-            (= t ")")
+          (or (= t ")") (= t "]"))
             (do (let [top-vec (first stack)
                       stack (rest stack)]
+                  (println "stack")
+                  (println stack)
                   (if (empty? stack)
                     top-vec
                     (do (let [next-vec (first stack) stack (rest stack)]
+                          (println "next-vec")
+                          (println next-vec)
+                          (println "top-vec")
+                          (println top-vec)
                           (recur (inc i) (into [] (cons (conj next-vec top-vec) stack))))))))
-            :else 
-            (do (let [curr-vec (first stack)]
-                (recur (inc i) (assoc stack 0 (into [] (conj curr-vec (tag t))))))))))))
+          (= t "[") (recur (inc i) (into [] (cons {:type :list :value []} stack)))
+          :else 
+            (do (let [curr-struct (first stack)]
+                (recur (inc i) (assoc stack 0 (insert-into-top curr-struct (tag t)))))))))))
 
 (defn remove-blanks
   [string]
-    (filter (fn[x] (not (empty? x))) (str/split string #"\\s")))
+    (filter (fn[x] (not (empty? x))) (str/split string #"\s")))
 
 (defn tokenize
   [string]
-  (let [spaced-out (remove-blanks (str/replace string #"[()]" #(str " " % " ")))]
-    spaced-out))
+    (let [spaced-out (remove-blanks (str/replace string #"[\[\]()]" #(str " " % " ")))]
+      spaced-out))
 
 ;; EVALUATING
 
 (defn get-type
   [expr]
   (cond 
-    (= clojure.lang.PersistentVector (type expr)) :call
-    (= :literal (:type expr)) :literal))
+    (is-vector? expr) :call
+    (= :literal (:type expr)) :literal
+    (= :list (:type expr)) :list))
 
 (defmulti evaluate get-type)
 
@@ -130,9 +188,7 @@
   [expr]
   (:value expr))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!")
-  (println (vectorify (tokenize "(+ 1 2)"))))
+(defmethod evaluate :list
+  [expr]
+  (map #(evaluate %) (:value expr)))
 
