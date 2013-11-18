38d14f211aa63885579cd735cfc61fc727141a47
Rupa Shankar <rupsshankar@gmail.com>
2013-11-18 14:58:54 -0500

    Works with if statements

--- src/interpreter/core.clj
+++ src/interpreter/core.clj
@@ -2,7 +2,7 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
-(def operator-map {"+" + "-" - "*" * "/" /})
+(def operator-map {"+" + "-" - "*" * "/" / "==" == ">" > "<" < "<=" <= ">=" >=})
 (def bool-map {"true" true "false" false})
 (def builtin-map {
   "map" map
@@ -12,7 +12,8 @@
   "first" first
   "last" last
   "not" not
-  "rest" rest})
+  "rest" rest
+  "type" type})
 
 (defn is-vector?
   [item]
@@ -31,20 +32,26 @@
     (Integer/parseInt token)
     (catch Exception e
       nil)))
-  
+
+(defn is-if?
+  [token]
+  (= token "if"))
+
 (defn tag
   [token]
   (let [operator (operator-map token)
         str-literal (str-literal token)
         num-literal (num-literal token)
         bool (bool-map token)
-        builtin (builtin-map token)]
+        builtin (builtin-map token)
+        is-if (is-if? token)]
   (cond
     (not (nil? operator)) {:type :operator :value operator}
     (not (nil? str-literal)) {:type :literal :value str-literal}
     (not (nil? num-literal)) {:type :literal :value num-literal}
     (not (nil? bool)) {:type :bool :value bool}
     (not (nil? builtin)) {:type :builtin :value builtin}
+    (not (nil? is-if)) {:type :if}
     :else {:type :identifier :value token})))
 
 ;; TOKENIZING
@@ -114,25 +121,54 @@
 
 (defn get-type
   [expr]
+  ;; (println expr)
   (cond 
-    (is-vector? expr) :call
     (= :literal (:type expr)) :literal
-    (= :list (:type expr)) :list))
+    (= :list (:type expr)) :list
+    (= :if (:type (first expr))) :if
+    :else :call))
 
 (defmulti evaluate get-type)
 
+(defmethod evaluate :if
+  [[if-obj cond true-expr false-expr :as expr]]
+  ;; (println "IF")
+  ;; (println expr)
+  (if (evaluate cond)
+    (evaluate true-expr)
+    (if false-expr
+      (evaluate false-expr))))
+
 (defmethod evaluate :call
   [expr]
+  ;; (println "CALL")
+  ;; (println expr)
+  ;; (println (:type (first expr)))
   (let [function (first expr)]
     (apply (function :value) (map #(evaluate %) (rest expr)))))
 
 (defmethod evaluate :literal
   [expr]
+  ;; (println "LITERAL")
+  ;; (println expr)
   (:value expr))
 
 (defmethod evaluate :list
   [expr]
   (map #(evaluate %) (:value expr)))
+
+(defn run
+  [line]
+  (evaluate (vectorify (tokenize line))))
+
+;; (defn -main
+;;   [& args]
+;;   (clojure.main/repl :init (fn [] (println "Little Lisp interpreter starting up"))
+;;                      :eval (fn [x] (run x))
+;;                      :prompt (fn [] (print ">> "))
+;;                      :read (fn [request-prompt request-exit]
+;;                              (let [form (clojure.main/repl-read request-prompt request-exit)]
+;;                                (if (= 'exit form) request-exit form)))))
 
 (defn -main
   [& args]
@@ -143,6 +179,10 @@
       (println "Quitting.")
       (do
         (println ">> " line)
-        (println (evaluate (vectorify (tokenize line))))
+        (let [vec (vectorify (tokenize line))]
+          ;; (println "VEC")
+          ;; (println vec)
+          (println (evaluate (vectorify (tokenize line)))))
         (recur (read-line))))))
+
 
